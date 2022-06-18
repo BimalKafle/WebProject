64f401a644b5bd25b0b13941a4ae21d77a81ab2b
BimalKafle <bkafle25@gmail.com>
2022-06-18 16:19:32 +0545

    Scope helper and repository created

---
+++ WebProject/E-Ticketing.CoreLayer/Src/Repository/VehicleCategoryRepositoryInterface.cs
@@ -0,0 +1,18 @@
+﻿using E_Ticketing.CoreLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Ticketing.CoreLayer.Repository
+{
+    public interface VehicleCategoryRepositoryInterface
+    {
+        public Task Insert(Vehicle_Category category);
+        public Task Update(Vehicle_Category category);
+        public Task<Vehicle_Category> GetById(long Id);
+        public Task<IEnumerable<Vehicle_Category>> GetAll();
+        
+    }
+}
---
+++ WebProject/E-Ticketing.CoreLayer/Src/Repository/VehicleSeatRepositoryInterface.cs
@@ -0,0 +1,18 @@
+﻿using E_Ticketing.CoreLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Ticketing.CoreLayer.Repository
+{
+    public interface VehicleSeatRepositoryInterface
+    {
+        public Task Insert(Vehicle_Seat seat);
+        public Task Update(Vehicle_Seat seat);
+        public Task<Vehicle_Seat> GetById(long Id);
+        public Task<IEnumerable<Vehicle_Seat>> GetAll();
+        public Task<IEnumerable<Vehicle_Seat>> GetAvailableSeat();
+    }
+}
